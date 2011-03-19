0e97a876e63238381b0504ceada3e49c960630c1
Eronarn Palazzo <eronarn@gmail.com>
2011-03-19 15:11:46 -0400

    Key input stuff.

--- src/info/eronarn/glyph/ui/Keys.java
+++ src/info/eronarn/glyph/ui/Keys.java
@@ -3,6 +3,7 @@ package info.eronarn.glyph.ui;
 import info.eronarn.glyph.Main;
 import info.eronarn.glyph.actor.Player;
 import info.eronarn.glyph.game.Game;
+import info.eronarn.glyph.game.EnumData.Command;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -27,11 +28,11 @@ public abstract class Keys {
                 case KeyEvent.VK_DOWN:  Player.Move (0,1);  break;
                 case KeyEvent.VK_PERIOD:  Player.Move (0,0); break;
                 // Item-y commands.
-                case KeyEvent.VK_D: Player.Drop(); break;
+                case KeyEvent.VK_D: Player.Extended(Command.drop); break;
                 case KeyEvent.VK_G: // fallthrough
                 case KeyEvent.VK_COMMA: Player.Get(); break;
-                case KeyEvent.VK_E: Player.Equip(); break;
-                case KeyEvent.VK_U: Player.Unequip(); break;
+                case KeyEvent.VK_E: Player.Extended(Command.equip); break;
+                case KeyEvent.VK_U: Player.Extended(Command.unequip); break;
             }
             if (Game.ready) {
             	Game.ready = false;
@@ -42,7 +43,33 @@ public abstract class Keys {
 
 		};
 	
-		public static void GetCommand() {
-	          Main.w.addKeyListener(action);
+	public final static KeyAdapter letter = new KeyAdapter() {
+		public void keyPressed(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+				Main.w.removeKeyListener(letter);
+				Player.Extended(Command.abort);
+				Main.w.addKeyListener(action);
+			} else if (Player.Extended(e.getKeyChar())) {
+				Main.w.removeKeyListener(letter);
+				Player.Extended(Command.done);
+				Main.w.addKeyListener(action);
+			}
+
+			if (Game.ready) {
+				Game.ready = false;
+				Main.w.removeKeyListener(action);
+				Game.RunQueue();
+			}
 		}
+
+	};
+
+	public static void GetCommand() {
+		Main.w.addKeyListener(action);
+	}
+
+	public static void GetLetter() {
+		Main.w.removeKeyListener(action);
+		Main.w.addKeyListener(letter);
+	}
 }
